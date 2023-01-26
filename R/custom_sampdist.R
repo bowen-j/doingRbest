93f6e66f4d2bfc77df887e1bf0bd445c76bc5d74
bowen-j <66271364+bowen-j@users.noreply.github.com>
2023-01-26 16:20:20 -0500

    Add files via upload

--- R/custom_sampdist.R
+++ R/custom_sampdist.R
@@ -11,7 +11,7 @@
 #' @export
 #' 
 #' @examples
-#'  Simple Regression Sampling Distribution(s)
+#' # Simple Regression Sampling Distribution(s)
 #' model <- regress(disp ~ mpg, data = mtcars)
 #' sampdist(model)
 #' # Multiple Regression Sampling Distribution(s)
@@ -19,6 +19,25 @@
 #' sampdist(model)
 #' # Moderated Regression Sampling Distribution(s)
 #' model <- regress(disp ~ mpg*wt, data = mtcars)
+#' sampdist(model)
+#' # Single Sample t-test Sampling Distribution(s)
+#' model <- ttest(outcome = "disp", data = mtcars, type = "s", mu = 100)
+#' sampdist(model, outcome = "Fuel Displacement")
+#' # Independent Samples t-test Sampling Distribution(s)
+#' model <- ttest(outcome = "hardness", predictor = "location", data = PASWR2::APPLE, type = "independent")
+#' sampdist(model)
+#' # Dependent Samples t-test Sampling Distribution(s) for "wide" format data
+#' early <- c(200.1, 190.9, 192.7, 213, 241.4, 196.9, 172.2, 185.5, 205.2, 193.7)
+#' late <- c(392.9, 393.2, 345.1, 393, 434, 427.9, 422, 383.9, 392.3, 352.2)
+#' my_data <- data.frame(early, late)
+#' model <- ttest(outcome = "weight", predictor = "group", data = my_data, data_format = "wide", type = "dependent", cols = c("early", "late"))
+#' sampdist(model)
+#' # Dependent Samples t-test Sampling Distribution(s) for "long" format data
+#' SubID <- factor(sort(rep(1:10,2)))
+#' Grp <- factor(rep(c("Ctrl","Trt"),10))
+#' DV <- rnorm(20,10,2)
+#' the_data <- data.frame(SubID, Grp, DV)
+#' model <- ttest(outcome = "DV", predictor = "Grp", data = the_data, data_format = "long", type = "dependent")
 #' sampdist(model)
 
 sampdist <- function(model = NULL,
